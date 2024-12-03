db771f12ba36cd25fdd519b9d5aa447746d4f231
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-12-03 19:27:08 +0600

    partition list :metro:

--- partion list/partition-list.java
+++ partion list/partition-list.java
@@ -1,13 +1,4 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+
 class Solution {
     public ListNode partition(ListNode head, int x) {
         ListNode small = new ListNode(0), higher = new ListNode(0);
@@ -28,3 +19,5 @@ class Solution {
         return small.next;
     }
 }
+
+// partition list
