067e5d9f74cb333ec579abf6b764af1e96d319e2
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-12-03 19:27:55 +0600

    partition list :rainbow:

--- partion list/partition-list.py
+++ partion list/partition-list.py
@@ -1,8 +1,4 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
 class Solution:
     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
         small, higher = ListNode(0), ListNode(0)
@@ -18,3 +14,4 @@ class Solution:
         higherHead.next = None
         smallHead.next = higher.next
         return small.next
+    # partition list
