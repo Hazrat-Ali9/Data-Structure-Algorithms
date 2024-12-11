b08762067a0f8d5f1594df8048f99f5cbd300ddd
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-11 07:57:31 -0600

    implement stack quarry :falafel:

--- implement-stack-using-queues/implement-stack-using-queues.java
+++ implement-stack-using-queues/implement-stack-using-queues.java
@@ -25,11 +25,4 @@ class MyStack {
     }
 }
 
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * boolean param_4 = obj.empty();
- */
+// Implement stack quarry
