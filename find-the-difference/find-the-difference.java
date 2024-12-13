05e4b4bf434bde734fe801ef80dbfd3942143b19
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-12 21:50:31 -0600

    find the difference :orangutan:

---
+++ find-the-difference/find-the-difference.java
@@ -0,0 +1,8 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+        char ans = 0;
+        for(char x : s.toCharArray()) ans ^= x;
+        for(char x : t.toCharArray()) ans ^= x;
+        return ans;
+    }
+}
