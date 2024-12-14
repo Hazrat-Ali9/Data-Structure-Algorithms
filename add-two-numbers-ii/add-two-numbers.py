93ea32c1143501c2a2f794bc8be7c2ea93039f66
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-14 09:09:15 -0600

    add two numbers python :cupcake:

--- add-two-numbers-ii/add-two-numbers.py
+++ add-two-numbers-ii/add-two-numbers.py
@@ -1,8 +1,4 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         l1 = self.reverseList(l1)
@@ -31,3 +27,5 @@ class Solution:
             prev = head
             head = temp
         return prev
+
+        # add two numbers
