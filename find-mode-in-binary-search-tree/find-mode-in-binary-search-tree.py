dd4c1fe5c5280421fb1508679c9ad8bac6f7539d
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-13 22:34:03 -0600

    find mode in binary search python :service_dog:

--- find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
+++ find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
@@ -1,9 +1,4 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# find mode in binary search
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
         mp = defaultdict(int)
