b4377bade45a9be675ef4d4d0acb880eb07bacfd
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-15 11:19:07 +0600

    largest element in stream :world_map:

--- kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
+++ kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.cpp
@@ -14,8 +14,4 @@ public:
     }
 };
 
-/**
- * Your KthLargest object will be instantiated and called as such:
- * KthLargest* obj = new KthLargest(k, nums);
- * int param_1 = obj->add(val);
- */
+// Largest element
