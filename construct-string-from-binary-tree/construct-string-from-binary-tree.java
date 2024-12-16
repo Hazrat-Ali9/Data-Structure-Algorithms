c239c6bd1667940aab586fd2cb64b46d654d91ff
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-15 22:50:10 -0600

    contract string binary java :euro_banknote:

--- construct-string-from-binary-tree/construct-string-from-binary-tree.java
+++ construct-string-from-binary-tree/construct-string-from-binary-tree.java
@@ -1,17 +1,4 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
+
  */
 class Solution {
     public String tree2str(TreeNode root) {
@@ -24,3 +11,5 @@ class Solution {
         return ans.toString();
     }
 }
+
+// Construct String
