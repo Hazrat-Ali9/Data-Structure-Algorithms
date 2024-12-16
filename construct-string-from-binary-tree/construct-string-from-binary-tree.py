291e96356e1d5dd35796c46696cf702ce3496835
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-15 22:50:50 -0600

    contract string binary python :bell:

--- construct-string-from-binary-tree/construct-string-from-binary-tree.py
+++ construct-string-from-binary-tree/construct-string-from-binary-tree.py
@@ -1,9 +1,5 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+
+
 class Solution:
     def tree2str(self, root: Optional[TreeNode]) -> str:
         if not root: return ""
@@ -12,3 +8,5 @@ class Solution:
         elif root.right: ans += "()"
         if root.right: ans += "(" + self.tree2str(root.right) + ")"
         return ans
+
+        # Construct String
