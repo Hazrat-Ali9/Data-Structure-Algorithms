cff158168de8bd48656b4f729e187ba4017f07b5
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-15 22:58:20 -0600

    Finf the smallest letter grather :wind_chime:

---
+++ find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int l = 0, r = letters.size()-1, m;
+        while(l <= r){
+            m = (l+r)/2;
+            if(letters[m] <= target) l = m+1;
+            else r = m-1;
+        }
+        return l == letters.size() ? letters[0] : letters[l];
+    }
+};
