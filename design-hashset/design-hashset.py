5b92d224fba268007653142db8021e46d330f806
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-15 22:55:59 -0600

    Design Hashset python :socks:

--- design-hashset/design-hashset.py
+++ design-hashset/design-hashset.py
@@ -12,8 +12,4 @@ class MyHashSet:
         return self.s[key] == 1
 
 
-# Your MyHashSet object will be instantiated and called as such:
-# obj = MyHashSet()
-# obj.add(key)
-# obj.remove(key)
-# param_3 = obj.contains(key)
+# design hashset
