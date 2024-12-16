61fdb77a91ef580615298b346604be481fed5eae
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-15 22:54:43 -0600

    Design Hashset :backpack:

--- design-hashset/design-hashset.cpp
+++ design-hashset/design-hashset.cpp
@@ -15,10 +15,4 @@ public:
     }
 };
 
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet* obj = new MyHashSet();
- * obj->add(key);
- * obj->remove(key);
- * bool param_3 = obj->contains(key);
- */
+// Design hashset
