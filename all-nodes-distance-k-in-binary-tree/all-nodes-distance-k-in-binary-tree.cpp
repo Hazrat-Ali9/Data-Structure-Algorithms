77544b518b944f42815f8df37f09e523cd70b61a
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-20 11:50:09 +0600

    all nodes distances k in binary  🛹

---
+++ all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<TreeNode*,TreeNode*>parent;
+        getparentbyBFS(parent,root);
+        queue<TreeNode*>q;
+        unordered_map<TreeNode*,bool>vis;
+        vis[target]=true;
+        q.push(target);
+        int lev=0;
+        while(!q.empty()){
+            int n=q.size();
+            if(lev==k)
+                break;
+            lev++;
+            while(n--){
+                TreeNode*curr=q.front();
+                q.pop();
+                if(curr->left&&!vis[curr->left]){
+                    vis[curr->left]=true;
+                    q.push(curr->left);
+                }
+                 if(curr->right&&!vis[curr->right]){
+                    vis[curr->right]=true;
+                    q.push(curr->right);
+                }
+                if(parent[curr]&&!vis[parent[curr]]){
+                    vis[parent[curr]]=true; 
+                    q.push(parent[curr]);
+                }
+            }
+        }
+        vector<int>ans;
+        while(q.size()>0){
+            ans.push_back(q.front()->val);
+            q.pop();
+        }
+        return ans;
+    }
+    void getparentbyBFS(unordered_map<TreeNode*,TreeNode*>&parent,TreeNode*root){
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            while(n--){
+                TreeNode*curr=q.front();
+                q.pop();
+                if(curr->left){
+                    parent[curr->left]=curr;
+                    q.push(curr->left);
+                }
+                if(curr->right){
+                      parent[curr->right]=curr;
+                    q.push(curr->right);
+                }
+            }
+        }
+    }
+};
