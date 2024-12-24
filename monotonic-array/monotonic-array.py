5465f1383aaea9bded6c8f0ac1604bca2acad86a
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-24 12:05:46 +0600

    Monotonic Array python 🎳

--- monotonic-array/monotonic-array.py
+++ monotonic-array/monotonic-array.py
@@ -1,3 +1,4 @@
 class Solution:
     def isMonotonic(self, nums: List[int]) -> bool:
         return all(nums[i+1]>=nums[i] for i in range(len(nums)-1)) or all(nums[i+1]<=nums[i] for i in range(len(nums)-1))
+        # Monotonic Array in python
