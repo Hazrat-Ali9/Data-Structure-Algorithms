ed8600319395e15d7e8bac29675e9a16258e9f76
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-24 12:45:25 +0600

    full binary tree java 🥁

--- possible-full-binary-trees/possible-full-binary-trees.java
+++ possible-full-binary-trees/possible-full-binary-trees.java
@@ -1,18 +1,4 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// Full Binary Tree
 class Solution {
     public List<TreeNode> allPossibleFBT(int n) {
         return f(n);
