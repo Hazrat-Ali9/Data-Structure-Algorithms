2aff7c71edafbe4f9264953bf4783771b469e06c
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-24 12:37:40 +0600

    peak index in Moutain array 🎩

---
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l, r = 0, len(arr)
+        while l < r:
+            m = (l+r)//2
+            if arr[m] < arr[m+1]:
+                l = m+1
+            else: r = m
+        return l
