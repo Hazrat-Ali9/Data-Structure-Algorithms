820d34a46c9b9b42aff0349c31fb836d96d5cbb2
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-27 12:10:02 +0600

    maximum numbers of vertices to reach all nodes 🍟

---
+++ minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        int[] inDeg = new int[n];
+        Arrays.fill(inDeg, 0);
+        for(List<Integer> e: edges){
+            inDeg[e.get(1)]++;
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0; i<n; i++){
+            if(inDeg[i]==0){
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
