4b9f014d2fca441e365d0477bf33f9dbf4d18e3c
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-27 12:17:07 +0600

    path with maximum probability 🍺

--- path-with-maximum-probability/path-with-maximum-probability.cpp
+++ path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        unordered_map<int, vector<pair<int, double>>> graph;
-        for(int i=0; i<edges.size(); i++){
-            int u = edges[i][0], v = edges[i][1];
-            double pathProb = succProb[i];
-            graph[u].push_back({v, pathProb});
-            graph[v].push_back({u, pathProb});
-        }
-        vector<double> mxProb(n);
-        mxProb[start_node] = 1.0;
-        queue<int> q;
-        q.push(start_node);
-        while(!q.empty()){
-            int currNode = q.front(); q.pop();
-            for(auto x : graph[currNode]){
-                int nxtNode = x.first;
-                double pathProb = x.second;
-                if(mxProb[currNode]*pathProb > mxProb[nxtNode]){
-                    mxProb[nxtNode] = mxProb[currNode] * pathProb;
-                    q.push(nxtNode);
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start, int end) {
+        vector<double> maxProb(n, 0.0);
+        maxProb[start] = 1.0;
+        for (int i = 0; i < n - 1; i++) {
+            bool hasUpdate = false;
+            for (int j = 0; j < edges.size(); j++) {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double pathProb = succProb[j];
+                if (maxProb[u] * pathProb > maxProb[v]) {
+                    maxProb[v] = maxProb[u] * pathProb;
+                    hasUpdate = true;
+                }
+                if (maxProb[v] * pathProb > maxProb[u]) {
+                    maxProb[u] = maxProb[v] * pathProb;
+                    hasUpdate = true;
                 }
             }
+            if (!hasUpdate) {
+                break;
+            }
         }
-        return mxProb[end_node];
+        return maxProb[end];
+        // path with maximum probability
     }
 };
