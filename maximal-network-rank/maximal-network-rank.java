5614e523a2c1f4dc33971d128c69212d2dc163b2
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-29 08:40:18 +0600

    🧸 🎮

--- maximal-network-rank/maximal-network-rank.java
+++ maximal-network-rank/maximal-network-rank.java
@@ -5,6 +5,7 @@ class Solution {
             adj.computeIfAbsent(x[0], k -> new HashSet<>()).add(x[1]);
             adj.computeIfAbsent(x[1], k -> new HashSet<>()).add(x[0]);
         }
+        // maximum network rank
         int mxRank = 0;
         for(int node1=0; node1<n; node1++){
             for(int node2=node1+1; node2<n; node2++){
