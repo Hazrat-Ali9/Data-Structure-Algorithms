0f66e52052b581d68e313c965b6b8f4270dd0d88
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2024-12-31 06:39:37 +0600

    seat reservation manager java 🥮

--- seat-reservation-manager/seat-reservation-manager.java
+++ seat-reservation-manager/seat-reservation-manager.java
@@ -15,9 +15,4 @@ class SeatManager {
     }
 }
 
-/**
- * Your SeatManager object will be instantiated and called as such:
- * SeatManager obj = new SeatManager(n);
- * int param_1 = obj.reserve();
- * obj.unreserve(seatNumber);
- */
+// Seat Reservation manager
