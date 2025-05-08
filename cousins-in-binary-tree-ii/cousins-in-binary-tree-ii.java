2ebe9500f6bf81a99c8dc8b826aa66cae0fcbc1b
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-08 10:37:36 +0600

    cousin in binary 2 :bus:

--- cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.java
+++ cousins-in-binary-tree-ii/cousins-in-binary-tree-ii.java
@@ -1,18 +1,4 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// Cousin in binartry 2
 class Solution {
     public TreeNode replaceValueInTree(TreeNode root) {
         int prevLevelSum = root.val;
